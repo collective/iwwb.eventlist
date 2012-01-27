a9b65d67f81e746a79d4567c497f88560321cfef
jcerjak <jcerjak@termitnjak.si>
2012-01-27 10:50:43 +0100

    Enable searching also by zip code and update the schema invariants. Also reorder the form fields and improve styling.

--- src/iwwb/eventlist/interfaces.py
+++ src/iwwb/eventlist/interfaces.py
@@ -30,25 +30,17 @@ class IListEventsForm(Interface):
             'Berlin, etc.'),
         required=False,
     )
-    # Doesn't work ATM
-    #===========================================================================
-    # allWords = schema.Bool(
-    #    title=_(u'All keywords'),
-    #    description=_(u'Select this if you want to search for all keywords.'),
-    #    required=False,
-    # )
-    #===========================================================================
-    city = schema.TextLine(
-        title=_(u'City'),
-        description=_(u'Enter the city. Examples: Berlin, Bonn, etc.'),
-        required=False,
-    )
     startDate = schema.Date(
         title=_(u'Date start'),
         description=_(u'Enter the event start date.'),
         required=False,
         default=date.today(),
         constraint=check_year_constraint,
+    )
+    city = schema.TextLine(
+        title=_(u'City'),
+        description=_(u'Enter the city. Examples: Berlin, Bonn, etc.'),
+        required=False,
     )
     endDate = schema.Date(
         title=_(u'Date end'),
@@ -57,6 +49,27 @@ class IListEventsForm(Interface):
         default=date.today() + timedelta(7),
         constraint=check_year_constraint,
     )
+    zip = schema.TextLine(
+        title=_(u'Zip code'),
+        description=_(u'Enter the zip code.'),
+        required=False,
+    )
+    type = schema.Choice(
+        title=_(u'Event type'),
+        description=_(u'Select the event type.'),
+        vocabulary='iwwb.eventlist.vocabularies.EventTypes',
+        required=True,
+        default=0,
+    )
+    # Doesn't work ATM
+    #===========================================================================
+    # allWords = schema.Bool(
+    #    title=_(u'All keywords'),
+    #    description=_(u'Select this if you want to search for all keywords.'),
+    #    required=False,
+    # )
+    #===========================================================================
+
     # Doesn't work ATM
     #===========================================================================
     # startTimeRequired = schema.Bool(
@@ -66,31 +79,28 @@ class IListEventsForm(Interface):
     #    required=False,
     # )
     #===========================================================================
-    type = schema.Choice(
-        title=_(u'Event type'),
-        description=_(u'Select the event type.'),
-        vocabulary='iwwb.eventlist.vocabularies.EventTypes',
-        required=False,
-        default=0,
-    )
-    sort = schema.Choice(
-        title=_(u'Sort by'),
-        vocabulary='iwwb.eventlist.vocabularies.SortOptions',
-        required=False,
-        default='startTime',
-    )
+
+    # Doesn't work ATM
+    #===========================================================================
+    # sort = schema.Choice(
+    #    title=_(u'Sort by'),
+    #    vocabulary='iwwb.eventlist.vocabularies.SortOptions',
+    #    required=True,
+    #    default='startTime',
+    # )
+    #===========================================================================
 
     @invariant
     def check_enough_data_provided(obj):
         """Check that the user has provided enough data to perform the query."""
-        if not (obj.query or obj.city or obj.startDate):
+        if not (obj.query or obj.city or obj.zip or obj.startDate or obj.endDate):
             raise Invalid(_("You have to fill out at least one of required " \
-                "fields: Keywords, City or Event Start"))
+                "fields: Keywords, City, Zip code, Event Start, Event End"))
 
     @invariant
     def check_end_date_bigger_than_start(obj):
-        if obj.startDate > obj.endDate:
+        if (obj.startDate and obj.endDate) and (obj.startDate > obj.endDate):
             raise Invalid(_("End date must be smaller than start date"))
         return True
 
-IWWB_SEARCHABLE_FIELDS = ('query', 'city', 'startDate', 'endDate', 'type', 'sort',)
+IWWB_SEARCHABLE_FIELDS = ('query', 'city', 'zip', 'startDate', 'endDate', 'type', 'sort',)
