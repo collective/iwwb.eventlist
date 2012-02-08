473929dc79121fa8356a838dec5f9d0b45de6e92
Domen Kožar <domen@dev.si>
2012-02-08 22:19:17 +0100

    fixes #2

--- src/iwwb/eventlist/browser/eventlist.py
+++ src/iwwb/eventlist/browser/eventlist.py
@@ -71,7 +71,7 @@ class ListEventsForm(form.Form):
     @button.buttonAndHandler(_(u"Reset"))
     def reset_form(self, action):
         """Cancel button handler."""
-        url = self.context.portal_url() + "/@@eventlist"
+        url = self.context.absolute_url() + "/@@eventlist"
         self.request.response.redirect(url)
 
 
