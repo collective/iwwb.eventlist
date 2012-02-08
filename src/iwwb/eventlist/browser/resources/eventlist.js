9250c087a9fb3e846797cc1d96ad2b9872b43839
Domen Kožar <domen@dev.si>
2012-02-08 23:52:27 +0100

    garden

--- src/iwwb/eventlist/browser/resources/eventlist.js
+++ src/iwwb/eventlist/browser/resources/eventlist.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
-
     $("#example").dataTable({
-
         "sDom": 'T<"clear">lfrtip', // where in DOM to inject TableTools controls
         "oTableTools": {
             "sSwfPath": portal_url + "/++resource++jquery.datatables/extras/TableTools/media/swf/copy_cvs_xls.swf",
-            aButtons: [ "copy", "csv", "xls"]
+            aButtons: ["copy", "csv", "xls"]
         },
         "sPaginationType": "full_numbers",
         "iDisplayLength": 25
