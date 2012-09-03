54035627cf15aae6853e3f1cfcdcc74d47053eb9
jcerjak <jcerjak@termitnjak.si>
2012-09-03 11:52:04 +0200

    Delete unused code in interfaces.py.

--- src/iwwb/eventlist/interfaces.py
+++ src/iwwb/eventlist/interfaces.py
@@ -29,13 +29,6 @@ class IListEventsForm(Interface):
             'Berlin, etc.'),
         required=False,
     )
-#    county = schema.Choice(
-#        title=_(u'County'),
-#        description=_(u"Choose a county"),
-#        vocabulary='iwwb.eventlist.vocabularies.Counties',
-#        required=True,
-#        default='alle'
-#    )
     startMonth = schema.Date(
         title=_(u'Course Start'),
         description=_(u'Select the month where the course should start'),
@@ -43,25 +36,6 @@ class IListEventsForm(Interface):
         default=date.today(),
         constraint=check_year_constraint,
     )
-#    startDate = schema.Date(
-#        title=_(u'Date start'),
-#        description=_(u'Enter the event start date.'),
-#        required=False,
-#        default=date.today(),
-#        constraint=check_year_constraint,
-#    )
-#    city = schema.TextLine(
-#        title=_(u'City'),
-#        description=_(u'Enter the city. Examples: Berlin, Bonn, etc.'),
-#        required=False,
-#    )
-#    endDate = schema.Date(
-#        title=_(u'Date end'),
-#        description=_(u'Enter the event start date.'),
-#        required=False,
-#        default=date.today() + timedelta(7),
-#        constraint=check_year_constraint,
-#    )
     zipcity = schema.TextLine(
         title=_(u'Zip or City'),
         description=_(u'Enter the zip code or city.' ),
@@ -74,7 +48,6 @@ class IListEventsForm(Interface):
         required=True,
         default=0,
     )
-
     startTimeRequired = schema.Bool(
         title=_(u'Exclude events without dates'),
         description=_(u'If the event does not have the date information it '
@@ -82,25 +55,6 @@ class IListEventsForm(Interface):
         required=False,
         default=True,
     )
-
-    # Doesn't work ATM
-    #=========================================================================
-    # allWords = schema.Bool(
-    #    title=_(u'All keywords'),
-    #    description=_(u'Select this if you want to search for all keywords.'),
-    #    required=False,
-    # )
-    #=========================================================================
-
-    # Doesn't work ATM
-    #=========================================================================
-    # sort = schema.Choice(
-    #    title=_(u'Sort by'),
-    #    vocabulary='iwwb.eventlist.vocabularies.SortOptions',
-    #    required=True,
-    #    default='startTime',
-    # )
-    #=========================================================================
 
     @invariant
     def check_enough_data_provided(obj):
@@ -110,11 +64,6 @@ class IListEventsForm(Interface):
             raise Invalid(_("You have to fill out at least one of required " \
                 "fields: Keywords, Zip code or city, Event Start, County"))
 
-    #@invariant
-    #def check_end_date_bigger_than_start(obj):
-    #    if (obj.startDate and obj.endDate) and (obj.startDate > obj.endDate):
-    #        raise Invalid(_("End date must be smaller than start date"))
-    #    return True
 
 IWWB_SEARCHABLE_FIELDS = (
     'query', 'county', 'zipcity', 'startMonth', 'type', 'sort',)
