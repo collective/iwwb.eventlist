f2b2be1182d59ece80cac64dad826501f0f1d06b
Nejc Zupan <nejc.zupan@gmail.com>
2012-09-20 10:35:56 +0200

    merge static html into eventlist.pt

--- src/iwwb/eventlist/browser/resources/eventlist.js
+++ src/iwwb/eventlist/browser/resources/eventlist.js
@@ -108,3 +108,42 @@
 
     });
 }(jQuery));
+
+
+/*global document, jQuery */
+(function ($) {
+    "use strict";
+
+    var currCodes = [],   //list of currently selected area codes
+        $txtCodes;        //textarea displaying currently selected area codes
+
+    //If area code has already been selected, remove it from the list,
+    //otherwise add it. Update textarea content at the end.
+    function areaClicked(code) {
+        var idx = $.inArray(code, currCodes);
+
+        if (idx > -1) {
+            currCodes.splice(idx, 1);
+        } else {
+            currCodes.push(code);
+            currCodes = currCodes.sort();
+        }
+
+        $txtCodes.val(currCodes.join(", "));
+    }
+
+    $(document).ready(function () {
+        $txtCodes = $("#txtCodes");
+
+        //set onclick handlers for all area elements
+        $("#germany_map area").each(function () {
+            var code = $(this).attr("alt").split(" ")[1];
+
+            $(this).click(function (event) {
+                event.preventDefault();
+                areaClicked(code);
+            });
+        });
+    });
+
+}(jQuery));
