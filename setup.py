3dc576993a20aa0b29ded10655632a5de135bfc7
Wolfgang Thomas <thomas@syslab.com>
2014-11-04 21:35:07 +0100

    write version number directly into setup.py, since zest.releaser cannot cope with the abstraction

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = read('src', 'iwwb', 'eventlist', 'version.txt').strip()
+version = "0.2.2"
 
 setup(name='iwwb.eventlist',
       version=version,
